06ba583ce2097b1de4daf3eaf1acd8bb7b737fb4
“[Breno <manancialgd@gmail.com>
2025-06-26 17:50:16 +0100

    Fix 2 obj with same ID and not being able to play the game twice if quitting from pause menu.

--- Assets/Scripts/Main Menu/Buttons.cs
+++ Assets/Scripts/Main Menu/Buttons.cs
@@ -5,8 +5,10 @@ public class Buttons : MonoBehaviour
 {
     public void ChangeScene(string scene)
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(scene);
     }
+
     public void ExitGame()
     {
         Application.Quit();
